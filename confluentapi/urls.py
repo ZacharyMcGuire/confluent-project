050b852298fb3f8356e8e078476f8fa44c7b93bf
Zachary McGuire <zachary_mcguire@outlook.com>
2020-06-15 17:19:24 +1000

    added a dynamic openAPI schema and redoc documentation.

--- confluentapi/urls.py
+++ confluentapi/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path, include
+from django.views.generic import TemplateView
 from rest_framework.routers import DefaultRouter
+from rest_framework.schemas import get_schema_view
 from confluentapi import views
 
 
@@ -10,4 +12,15 @@ router.register(r'pages', views.PageViewSet)
 urlpatterns = [
     path('', include(router.urls)),
     path('auth/', include('rest_framework.urls'), name='auth'),
+    path('openapi/', get_schema_view(
+        title="Confluent Project",
+        description="API for a simple Confluence clone.",
+        version="0.1.0",
+        url='http://127.0.0.1:8000/api/',
+        urlconf='confluentapi.urls'
+    ), name='openapi-schema'),
+    path('docs/', TemplateView.as_view(
+        template_name='redoc.html',
+        extra_context={'schema_url': 'openapi-schema'}
+    ), name='redoc'),
 ]
